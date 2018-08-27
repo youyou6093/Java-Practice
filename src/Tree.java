3678ad498ee73c0287c9c32ee3ca1fd59e321fc7
Junjie Yang <youyou6093@gmail.com>
2018-08-27 01:38:14 -0400

    Add a few  tree examples.

---
+++ src/Tree.java
@@ -0,0 +1,26 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Tree {
+    public static TreeNode constructTree(Integer[] array) {
+        TreeNode root = new TreeNode(array[0]);
+        Deque<TreeNode> queue = new LinkedList<>();
+        int index = 1;
+        queue.offerLast(root);
+        while (index < array.length) {
+            TreeNode cur = queue.pollFirst();
+            if (cur == null) continue;
+            cur.left = array[index] == null ? null : new TreeNode(array[index]);
+            index++;
+            if (index >= array.length) break;
+            cur.right = array[index] == null ? null : new TreeNode(array[index]);
+            index++;
+            if (index >= array.length) break;
+            queue.offerLast(cur.left);
+            queue.offerLast(cur.right);
+        }
+        return root;
+
+    }
+}
---
+++ src/TreeTraversal.java
@@ -0,0 +1,93 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class TreeTraversal {
+    public static void main (String[] args){
+        Integer[] array = {5, 6, 7, 8, null, 10, 11};
+        TreeNode root = Tree.constructTree(array);
+        System.out.println(Arrays.toString(preOrder(root).toArray()));
+        System.out.println(Arrays.toString(inOrder(root).toArray()));
+        System.out.println(Arrays.toString(postOrder(root).toArray()));
+
+    }
+
+    public static void inOrderRecursive(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inOrderRecursive(root.left);
+        System.out.print(root.key);
+        inOrderRecursive(root.right);
+    }
+
+    public static List<Integer> preOrder(TreeNode root) {
+        List<Integer> ret = new ArrayList<>();
+        if (root == null) {
+            return ret;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            ret.add(cur.key);
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+        }
+        return ret;
+    }
+
+    public static List<Integer> inOrder(TreeNode root) {
+        List<Integer> ret = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                root = stack.pop();
+                ret.add(root.key);
+                root = root.right;
+            }
+        }
+        return ret;
+    }
+
+    public static List<Integer> postOrder(TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        List<Integer> ret = new ArrayList<>();
+        if (root == null) {
+            return ret;
+        }
+        TreeNode prev = null;
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.peek();
+            if (prev == null || cur == prev.left || cur == prev.right) {
+                if (cur.left != null) {
+                    stack.push(cur.left);
+                } else if (cur.right != null) {
+                    stack.push(cur.right);
+                } else {
+                    ret.add(cur.key);
+                    stack.pop();
+                }
+            } else if (prev == cur.left) {
+                if (cur.right != null) {
+                    stack.push(cur.right);
+                } else {
+                    ret.add(cur.key);
+                    stack.pop();
+                }
+            } else  {
+                ret.add(cur.key);
+                stack.pop();
+            }
+            prev = cur;
+        }
+        return  ret;
+    }
+}
